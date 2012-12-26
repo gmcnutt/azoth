90f7b64189887b61500bb8dc5bce57285dba614e
Gordon McNutt <gmcnutt@cableone.net>
2012-12-25 18:00:29 -0700

    Updated description, removed pgu dependency

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'Curses-like hax2 client',
+    'description': 'Single-player RPG game',
     'author': 'Gordon McNutt',
     'url' : 'URL to get it at',
     'download_url': 'Where to download it.',
     'author_email': 'gmcnutt@cableone.net',
     'version': '0.1',
-    'install_requires': ['nose', 'pygame', 'pgu'],
+    'install_requires': ['nose', 'pygame'],
     'packages': ['azoth'],
     'scripts': [],
     'name': 'azoth'
