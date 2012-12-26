e24c061edb7c9f65f11293879ee3aa7e21e9577b
Gordon McNutt <gmcnutt@cableone.net>
2012-12-25 18:45:09 -0700

    Splash screen working

--- azoth.py
+++ azoth.py
@@ -1,25 +1,9 @@
 #!/usr/bin/python
 
 import argparse
-import classes
-import cPickle
-import colors
-import config
-import gui
-import inspect
-import json
 import logging
 import pygame
-import reagents
 import os
-import session
-import sprites
-import sys
-import terrain
-
-
-class AzothObject(object):
-    name = "azoth object"  # placeholder
 
 
 if __name__ == "__main__":
@@ -41,61 +25,18 @@ if __name__ == "__main__":
     screen = pygame.display.set_mode((640, 480), 0)
     pygame.key.set_repeat(500, 10) # XXX: put in config.py
 
-    # Load the sprites.
-    sheets = {}
-    sheet_table = json.loads(open(config.SHEET_DATA_FILE).read())
-    for k, v in sheet_table.items():
-        sheets[k] = sprites.Sheet(*v)
+    # Show the splash screen.
+    splash = pygame.image.load('resources/splash.png')
+    
+    main_surface = pygame.display.get_surface()
+    main_surface.blit(splash, (0, 0))
+    pygame.display.flip()
 
-    all_sprites = {}
-    sprite_table = json.loads(open(config.SPRITE_DATA_FILE).read())
-    for k, v in sprite_table.items():
-        sheet = sheets[v[0]]
-        frames = v[1]
-        start = v[2]
-        wave = v[3]
-        facings = v[4]
-        if not wave:
-            sprite = sprites.AnimatedSprite(sheet, frames, start, facings=facings)
-        else:
-            sprite = sprites.WaveSprite(sheet, start)
-        all_sprites[k] = sprite
-
-    # Initialize terrains
-    all_terrains = {}
-    terrain.HeavyForest.sprite = all_sprites['forest']
-    terrain.Forest.sprite = all_sprites['trees']
-    terrain.Grass.sprite = all_sprites['grass']
-    terrain.Trail.sprite = all_sprites['trail_f']
-    terrain.RockWall.sprite = all_sprites['wall_stone']
-    terrain.CobbleStone.sprite = all_sprites['cobblestone']
-    terrain.Window.sprite = all_sprites['window_in_stone']
-    terrain.CounterTop.sprite = all_sprites['counter_1x1']
-    terrain.FirePlace.sprite = all_sprites['fireplace']
-    terrain.Boulder.sprite = all_sprites['boulder']
-    terrain.Bog.sprite = all_sprites['bog']
-    terrain.Water.sprite = all_sprites['shoals']
-
-    # Show them in a window for dev.
-    for name, obj in inspect.getmembers(terrain):
-        if inspect.isclass(obj):
-            if hasattr(obj, 'sprite'):
-                all_terrains[obj.name] = obj
-    #gui.TerrainGridViewer(sorted(all_terrains.items())).run()
-
-    # Initialize reagents.
-    all_reagents = {}
-    for name, obj in inspect.getmembers(reagents):
-        if (inspect.isclass(obj) and issubclass(obj, reagents.AzothObject) 
-            and (obj != reagents.AzothObject)):
-            obj.sprite = all_sprites[obj.__name__]
-            all_reagents[obj.name] = obj
-    gui.ObjectListViewer(sorted(all_reagents.items())).run()
-
-    # Load the session.
-    session = session.load(open(cmdargs.start))
-    sector = session.world
-    place_view = gui.PlaceViewer(sector)
-    place_view.run()
-
-
+    doquit = False
+    while not doquit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                doquit = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    doquit = True
