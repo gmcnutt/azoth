b9e4b61e3bced6ab3b1d8f389c0667186690740a
Gordon McNutt <gmcnutt@cableone.net>
2012-12-29 11:33:13 -0700

    Forgot to add moved files

---
+++ build.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+from azoth import controller, being, weapon, place, session, terrain, terrainmap
+import cPickle
+import sys
+
+sys.path.append('../')
+
+#import hax2
+#from hax2 import being, pragma, rules, weapon
+
+sesh = session.Session()
+
+tmap = terrainmap.load_from_nazghul_scm('haxima/scm/gregors-hut.scm')
+sesh.world = place.Sector(name='gh', default_terrain=terrain.Grass)
+sesh.world.blit_terrain_map(0, 0, tmap)
+
+sesh.player = being.Human('Scaramouche')
+sesh.player.controller = controller.Player(sesh.player, sesh)
+sesh.hax2.put_being_on_map(sesh.player, sesh.world, 0, 5)
+
+sword = weapon.Sword()
+sesh.hax2.put_item_on_map(sword, sesh.world, 3, 3)
+
+troll = being.Troll('Skoligidornifor')
+troll.controller = controller.Follow(sesh.player, troll, sesh)
+sesh.hax2.put_being_on_map(troll, sesh.world, 0, 6)
+
+savefile = open('start.p', 'w')
+cPickle.dump(sesh, savefile)
+savefile.close()
